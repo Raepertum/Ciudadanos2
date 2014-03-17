2d0c6bbdab3781ab716f8e49d0fe9c029893d38c
Raepertum <cejamutante@hotmail.com>
2014-03-18 00:25:06 +0100

    Reducida la clase de Menú Almacén a un tamaño más manejable. Creada clase interna dentro de variables de juego para un mejor control. Lo primero que hay que hacer mañana es hacer que los labels se vayan añadiendo a las arrays que ya están creadas en menú almacén. Luego hacer que se sincronicen con la clase variables de juego (Iteraciones entre dos arrays del mismo tamaño, debería ser fácil). Si da tiempo, crear los botones dentro del menú almacén que permitan ir viendo otros submenús.

--- Ciudadanos/src/com/virtualboardgames/ciudadanos/MenuAlmacen.java
+++ Ciudadanos/src/com/virtualboardgames/ciudadanos/MenuAlmacen.java
@@ -20,73 +20,12 @@ public class MenuAlmacen extends Stack{
 	//Instancias de las imágenes (Una para el fondo, el resto para los iconos)	
 	Image fondomenualmacen;
 	
-	//Instancias de los textos, todos dentro de un label
-	Label titulo;
-	Label espacio;
-	Label cantidadentrames;
-	Label cantidadsalemes;
-	Label resultadocantidadmes;
-	Label cantidadentraano;
-	Label cantidadsaleano;
-	Label resultadocantidadano;
-	Label cantidadalmacenada;
-	Label cantidadexpirames;
-	Label trigo;
-	Label pmtrigo;
-	Label gmtrigo;
-	Label nmtrigo;
-	Label patrigo;
-	Label gatrigo;
-	Label natrigo;
-	Label retrigo;
-	Label cptrigo;
-	Label fruta;
-	Label pmfruta;
-    Label gmfruta;
-    Label nmfruta;
-    Label pafruta;
-    Label gafruta;
-    Label nafruta;
-    Label refruta;
-    Label cpfruta;
-	Label carne;
-	Label pmcarne;
-	Label gmcarne;
-	Label nmcarne;
-	Label pacarne;
-	Label gacarne;
-	Label nacarne;
-	Label recarne;
-	Label cpcarne;
-	Label carneensalazon;
-	Label pmcarnesal;
-	Label gmcarnesal;
-	Label nmcarnesal;
-	Label pacarnesal;
-	Label gacarnesal;
-	Label nacarnesal; 
-	Label recarnesal;
-	Label cpcarnesal;
-	Label pescado;
-	Label pmpesc;
-	Label gmpesc;
-	Label nmpesc;
-	Label papesc;
-	Label gapesc;
-	Label napesc;
-	Label repesc;
-	Label cppesc;
-	Label miel;
-	Label pmmiel;
-	Label gmmiel;
-	Label nmmiel;
-	Label pamiel;
-	Label gamiel;
-	Label namiel;
-	Label remiel;
-	Label cpmiel;
-		
+	//Arrays de labels que contienen texto
+	Label[] arraydelabelsstring;
 	
+	//Array de labels que contienen variables
+	Label[] arraydelabelsvars;
+		
 	//Estilos: Para no tener que estar escribiendo desde el principio
 	LabelStyle estilolabel;
 	
@@ -115,224 +54,125 @@ public class MenuAlmacen extends Stack{
     //Los estilos
     estilolabel = Texturasysonidos.texturasysonidos.estilosyactores.estilolabeldefault;
     
-    //Los labels    
-    titulo = new Label("ALMACÉN", estilolabel);
-    espacio = new Label("", estilolabel);
-	cantidadentrames = new Label("Prod/Mes", estilolabel);
-	cantidadsalemes = new Label("Gasto/Mes", estilolabel);
-	resultadocantidadmes = new Label("Neto mes", estilolabel);
-	cantidadentraano = new Label("Prod/Año", estilolabel);
-	cantidadsaleano = new Label("Gasto/Año", estilolabel);
-	resultadocantidadano = new Label("Neto año", estilolabel);
-	cantidadalmacenada = new Label("Reservas", estilolabel);
-	cantidadexpirames = new Label("Cad. Próx.", estilolabel);
-	trigo = new Label("Trigo", estilolabel);
-	pmtrigo = new Label(""+Variablesdejuego.variablesdejuego.pmtrigo, estilolabel);
-	gmtrigo = new Label(""+Variablesdejuego.variablesdejuego.gmtrigo, estilolabel);
-	nmtrigo = new Label(""+Variablesdejuego.variablesdejuego.nmtrigo, estilolabel);
-	patrigo = new Label(""+Variablesdejuego.variablesdejuego.patrigo, estilolabel);
-	gatrigo = new Label(""+Variablesdejuego.variablesdejuego.gatrigo, estilolabel);
-	natrigo = new Label(""+Variablesdejuego.variablesdejuego.natrigo, estilolabel);
-	retrigo = new Label(""+Variablesdejuego.variablesdejuego.retrigo, estilolabel);
-	cptrigo = new Label(""+Variablesdejuego.variablesdejuego.cptrigo, estilolabel);
-	fruta = new Label("Fruta", estilolabel);
-	pmfruta = new Label(""+Variablesdejuego.variablesdejuego.pmfruta, estilolabel);
-	gmfruta = new Label(""+Variablesdejuego.variablesdejuego.gmfruta, estilolabel);
-	nmfruta = new Label(""+Variablesdejuego.variablesdejuego.nmfruta, estilolabel);
-	pafruta = new Label(""+Variablesdejuego.variablesdejuego.pafruta, estilolabel);
-	gafruta = new Label(""+Variablesdejuego.variablesdejuego.gafruta, estilolabel);
-	nafruta = new Label(""+Variablesdejuego.variablesdejuego.nafruta, estilolabel);
-	refruta = new Label(""+Variablesdejuego.variablesdejuego.refruta, estilolabel);
-	cpfruta = new Label(""+Variablesdejuego.variablesdejuego.cpfruta, estilolabel);
-	carne = new Label("Carne", estilolabel);
-	pmcarne = new Label(""+Variablesdejuego.variablesdejuego.pmcarne, estilolabel);
-	gmcarne = new Label(""+Variablesdejuego.variablesdejuego.gmcarne, estilolabel);
-	nmcarne = new Label(""+Variablesdejuego.variablesdejuego.nmcarne, estilolabel);
-	pacarne = new Label(""+Variablesdejuego.variablesdejuego.pacarne, estilolabel);
-	gacarne = new Label(""+Variablesdejuego.variablesdejuego.gacarne, estilolabel);
-	nacarne = new Label(""+Variablesdejuego.variablesdejuego.nacarne, estilolabel);
-	recarne = new Label(""+Variablesdejuego.variablesdejuego.recarne, estilolabel);
-	cpcarne = new Label(""+Variablesdejuego.variablesdejuego.cpcarne, estilolabel);
-	carneensalazon = new Label("Carne sal.", estilolabel);
-	pmcarnesal = new Label(""+Variablesdejuego.variablesdejuego.pmcarnesal, estilolabel);
-	gmcarnesal = new Label(""+Variablesdejuego.variablesdejuego.gmcarnesal, estilolabel);
-	nmcarnesal = new Label(""+Variablesdejuego.variablesdejuego.nmcarnesal, estilolabel);
-	pacarnesal = new Label(""+Variablesdejuego.variablesdejuego.pacarnesal, estilolabel);
-	gacarnesal = new Label(""+Variablesdejuego.variablesdejuego.gacarnesal, estilolabel);
-	nacarnesal = new Label(""+Variablesdejuego.variablesdejuego.nacarnesal, estilolabel);
-	recarnesal = new Label(""+Variablesdejuego.variablesdejuego.recarnesal, estilolabel);
-	cpcarnesal = new Label(""+Variablesdejuego.variablesdejuego.cpcarnesal, estilolabel);
-	pescado = new Label("Pescado", estilolabel);
-	pmpesc = new Label(""+Variablesdejuego.variablesdejuego.pmpesc, estilolabel);
-	gmpesc = new Label(""+Variablesdejuego.variablesdejuego.gmpesc, estilolabel);
-	nmpesc = new Label(""+Variablesdejuego.variablesdejuego.nmpesc, estilolabel);
-	papesc = new Label(""+Variablesdejuego.variablesdejuego.papesc, estilolabel);
-	gapesc = new Label(""+Variablesdejuego.variablesdejuego.gapesc, estilolabel);
-	napesc = new Label(""+Variablesdejuego.variablesdejuego.napesc, estilolabel);
-	repesc = new Label(""+Variablesdejuego.variablesdejuego.repesc, estilolabel);
-	cppesc = new Label(""+Variablesdejuego.variablesdejuego.cppesc, estilolabel);
-	miel = new Label("Miel", estilolabel);
-	pmmiel = new Label(""+Variablesdejuego.variablesdejuego.pmmiel, estilolabel);
-	gmmiel = new Label(""+Variablesdejuego.variablesdejuego.gmmiel, estilolabel);
-	nmmiel = new Label(""+Variablesdejuego.variablesdejuego.nmmiel, estilolabel);
-	pamiel = new Label(""+Variablesdejuego.variablesdejuego.pamiel, estilolabel);
-	gamiel = new Label(""+Variablesdejuego.variablesdejuego.gamiel, estilolabel);
-	namiel = new Label(""+Variablesdejuego.variablesdejuego.namiel, estilolabel);
-	remiel = new Label(""+Variablesdejuego.variablesdejuego.remiel, estilolabel);
-	cpmiel = new Label(""+Variablesdejuego.variablesdejuego.cpmiel, estilolabel);
-	
-	
-	//La alineación de los labels
-	titulo.setAlignment(4);
-	cantidadentrames.setAlignment(1);
-	pmtrigo.setAlignment(1);
-	cantidadsalemes.setAlignment(1);
-	resultadocantidadmes.setAlignment(1);
-	cantidadentraano.setAlignment(1);
-	cantidadsaleano.setAlignment(1);
-	resultadocantidadano.setAlignment(1);
-	cantidadalmacenada.setAlignment(1);
-	cantidadexpirames.setAlignment(1);
-	trigo.setAlignment(1);
-	pmtrigo.setAlignment(1);
-	gmtrigo.setAlignment(1);
-	nmtrigo.setAlignment(1);
-	patrigo.setAlignment(1);
-	gatrigo.setAlignment(1);
-	natrigo.setAlignment(1);
-	retrigo.setAlignment(1);
-	cptrigo.setAlignment(1);
-	fruta.setAlignment(1);
-	pmfruta.setAlignment(1);
-    gmfruta.setAlignment(1);
-    nmfruta.setAlignment(1);
-    pafruta.setAlignment(1);
-    gafruta.setAlignment(1);
-    nafruta.setAlignment(1);
-    refruta.setAlignment(1);
-    cpfruta.setAlignment(1);
-	carne.setAlignment(1);
-	pmcarne.setAlignment(1);
-	gmcarne.setAlignment(1);
-	nmcarne.setAlignment(1);
-	pacarne.setAlignment(1);
-	gacarne.setAlignment(1);
-	nacarne.setAlignment(1);
-	recarne.setAlignment(1);
-	cpcarne.setAlignment(1);
-	carneensalazon.setAlignment(1);
-	pmcarnesal.setAlignment(1);
-	gmcarnesal.setAlignment(1);
-	nmcarnesal.setAlignment(1);
-	pacarnesal.setAlignment(1);
-	gacarnesal.setAlignment(1);
-	nacarnesal.setAlignment(1);
-	recarnesal.setAlignment(1);
-	cpcarnesal.setAlignment(1);
-	pescado.setAlignment(1);
-	pmpesc.setAlignment(1);
-	gmpesc.setAlignment(1);
-	nmpesc.setAlignment(1);
-	papesc.setAlignment(1);
-	gapesc.setAlignment(1);
-	napesc.setAlignment(1);
-	repesc.setAlignment(1);
-	cppesc.setAlignment(1);
-	miel.setAlignment(1);
-	pmmiel.setAlignment(1);
-	gmmiel.setAlignment(1);
-	nmmiel.setAlignment(1);
-	pamiel.setAlignment(1);
-	gamiel.setAlignment(1);
-	namiel.setAlignment(1);
-	remiel.setAlignment(1);
-	cpmiel.setAlignment(1);
-		
-	
+    //El array de labels
+    arraydelabelsstring = new Label[64];
+   
+    //Las tablas
+    
     //Empezamos por arriba a la izquierda, y establecemos los márgenes
-	tabledetexto.left().top();
-	tabledetexto.padLeft(20);
-	tabledetexto.padTop(20);
-	
-	//Añadimos el fondo
-	tabledefondo.add(fondomenualmacen);
-	
-	//Añadimos el label (Alternamos con row)
-	tabledetexto.left();
-	tabledetexto.add(titulo).width(anchocolumnas);
-	tabledetexto.row().height(80);
-	tabledetexto.add(espacio).width(anchocolumnas);
-	tabledetexto.add(cantidadentrames).width(anchocolumnas);
-	tabledetexto.add(cantidadsalemes).width(anchocolumnas);
-	tabledetexto.add(resultadocantidadmes).width(anchocolumnas);
-	tabledetexto.add(cantidadentraano).width(anchocolumnas);
-	tabledetexto.add(cantidadsaleano).width(anchocolumnas);
-	tabledetexto.add(resultadocantidadano).width(anchocolumnas);
-	tabledetexto.add(cantidadalmacenada).width(anchocolumnas);
-	tabledetexto.add(cantidadexpirames).width(anchocolumnas);
-	tabledetexto.row();
-	tabledetexto.add(trigo).width(anchocolumnas);
-	tabledetexto.add(pmtrigo).width(anchocolumnas);
-	tabledetexto.add(gmtrigo).width(anchocolumnas);
-	tabledetexto.add(nmtrigo).width(anchocolumnas);
-	tabledetexto.add(patrigo).width(anchocolumnas);
-	tabledetexto.add(gatrigo).width(anchocolumnas);
-	tabledetexto.add(natrigo).width(anchocolumnas);
-	tabledetexto.add(retrigo).width(anchocolumnas);
-	tabledetexto.add(cptrigo).width(anchocolumnas);
-	tabledetexto.row();
-	tabledetexto.add(fruta).width(anchocolumnas);
-	tabledetexto.add(pmfruta).width(anchocolumnas);
-	tabledetexto.add(gmfruta).width(anchocolumnas);
-	tabledetexto.add(nmfruta).width(anchocolumnas);
-	tabledetexto.add(pafruta).width(anchocolumnas);
-	tabledetexto.add(gafruta).width(anchocolumnas);
-	tabledetexto.add(nafruta).width(anchocolumnas);
-	tabledetexto.add(refruta).width(anchocolumnas);
-	tabledetexto.add(cpfruta).width(anchocolumnas);
-	tabledetexto.row();
-	tabledetexto.add(carne).width(anchocolumnas);
-	tabledetexto.add(pmcarne).width(anchocolumnas);
-	tabledetexto.add(gmcarne).width(anchocolumnas);
-	tabledetexto.add(nmcarne).width(anchocolumnas);
-	tabledetexto.add(pacarne).width(anchocolumnas);
-	tabledetexto.add(gacarne).width(anchocolumnas);
-	tabledetexto.add(nacarne).width(anchocolumnas);
-	tabledetexto.add(recarne).width(anchocolumnas);
-	tabledetexto.add(cpcarne).width(anchocolumnas);
-	tabledetexto.row();
-	tabledetexto.add(carneensalazon).width(anchocolumnas);
-	tabledetexto.add(pmcarnesal).width(anchocolumnas);
-	tabledetexto.add(gmcarnesal).width(anchocolumnas);
-	tabledetexto.add(nmcarnesal).width(anchocolumnas);
-	tabledetexto.add(pacarnesal).width(anchocolumnas);
-	tabledetexto.add(gacarnesal).width(anchocolumnas);
-	tabledetexto.add(nacarnesal).width(anchocolumnas);
-	tabledetexto.add(recarnesal).width(anchocolumnas);
-	tabledetexto.add(cpcarnesal).width(anchocolumnas);
-	tabledetexto.row();
-	tabledetexto.add(pescado).width(anchocolumnas);
-	tabledetexto.add(pmpesc).width(anchocolumnas);
-	tabledetexto.add(gmpesc).width(anchocolumnas);
-	tabledetexto.add(nmpesc).width(anchocolumnas);
-	tabledetexto.add(papesc).width(anchocolumnas);
-	tabledetexto.add(gapesc).width(anchocolumnas);
-	tabledetexto.add(napesc).width(anchocolumnas);
-	tabledetexto.add(repesc).width(anchocolumnas);
-	tabledetexto.add(cppesc).width(anchocolumnas);
-	tabledetexto.row();
-	tabledetexto.add(miel).width(anchocolumnas);
-	tabledetexto.add(pmmiel).width(anchocolumnas);
-	tabledetexto.add(gmmiel).width(anchocolumnas);
-	tabledetexto.add(nmmiel).width(anchocolumnas);
-	tabledetexto.add(pamiel).width(anchocolumnas);
-	tabledetexto.add(gamiel).width(anchocolumnas);
-	tabledetexto.add(namiel).width(anchocolumnas);
-	tabledetexto.add(remiel).width(anchocolumnas);
-	tabledetexto.add(cpmiel).width(anchocolumnas);
-
-}
-
+   	tabledetexto.left().top();
+   	tabledetexto.padLeft(20);
+   	tabledetexto.padTop(20);
+   	
+   	//Añadimos el fondo
+   	tabledefondo.add(fondomenualmacen);
+    
+    //Empezamos por la izquierda
+   	tabledetexto.left();
+   	
+    //Los labels    
+    
+    //La primera tanda es de strings
+	for(int i=0; i<10; i++){
+    Label label = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[i],estilolabel);
+    label.setAlignment(1);
+    tabledetexto.add(label).width(anchocolumnas);
+    if(i==0){
+    	tabledetexto.row().height(80);
+    }
+    }
+    
+	//Trigo
+    tabledetexto.row().height(60);
+    Label label = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[10],estilolabel);
+    label.setAlignment(1);
+    tabledetexto.add(label).width(anchocolumnas);
+    
+    //Las ints del trigo
+    for(int i=0; i<8; i++){
+    
+    Label label2 = new Label(""+Variablesdejuego.variablesdejuego.almacen.intsdealmacen[i],estilolabel);
+    label2.setAlignment(1);
+    tabledetexto.add(label2).width(anchocolumnas);
+    }
+    
+    //Fruta
+    tabledetexto.row().height(60);
+    Label label3 = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[11],estilolabel);
+    label3.setAlignment(1);
+    tabledetexto.add(label3).width(anchocolumnas);
+    
+    //Las ints de la fruta
+    for(int i=8; i<16; i++){
+    Label label4 = new Label(""+Variablesdejuego.variablesdejuego.almacen.intsdealmacen[i],estilolabel);
+    label4.setAlignment(1);
+    tabledetexto.add(label4).width(anchocolumnas);
+    }
+    
+    //Carne
+    tabledetexto.row().height(60);
+    Label label5 = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[12],estilolabel);
+    label5.setAlignment(1);
+    tabledetexto.add(label5).width(anchocolumnas);
+    
+    //Las ints de la carne
+    for(int i=16; i<24; i++){
+    Label label6 = new Label(""+Variablesdejuego.variablesdejuego.almacen.intsdealmacen[i],estilolabel);
+    label6.setAlignment(1);
+    tabledetexto.add(label6).width(anchocolumnas);
+    }
+    
+    //Carne salada
+    
+    tabledetexto.row().height(60);
+    Label label7 = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[13],estilolabel);
+    label7.setAlignment(1);
+    tabledetexto.add(label7).width(anchocolumnas);
+   
+    
+    //Las ints de la carne salada
+    
+    for(int i=24; i<32; i++){
+    Label label8 = new Label(""+Variablesdejuego.variablesdejuego.almacen.intsdealmacen[i],estilolabel);
+    label8.setAlignment(1);
+    tabledetexto.add(label8).width(anchocolumnas);
+    }
+    
+    //Pescado
+    
+    tabledetexto.row().height(60);
+    Label label9 = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[14],estilolabel);
+    label9.setAlignment(1);
+    tabledetexto.add(label9).width(anchocolumnas);
+   
+    
+    //Las ints del pescado
+    
+    for(int i=32; i<40; i++){
+    Label label10 = new Label(""+Variablesdejuego.variablesdejuego.almacen.intsdealmacen[i],estilolabel);
+    label10.setAlignment(1);
+    tabledetexto.add(label10).width(anchocolumnas);
+        }
+    
+    //Miel
+    
+    tabledetexto.row().height(60);
+    Label label11 = new Label(Variablesdejuego.variablesdejuego.almacen.stringsdealmacen[15],estilolabel);
+    label11.setAlignment(1);
+    tabledetexto.add(label11).width(anchocolumnas);
+   
+    
+    //Las ints de la miel
+    
+    for(int i=40; i<48; i++){
+    Label label12 = new Label(""+Variablesdejuego.variablesdejuego.almacen.intsdealmacen[i],estilolabel);
+    label12.setAlignment(1);
+    tabledetexto.add(label12).width(anchocolumnas);
+    }
+    
+   
+    
+ }  
 
 public void act(float deltatime){
 	
--- Ciudadanos/src/com/virtualboardgames/ciudadanos/Variablesdejuego.java
+++ Ciudadanos/src/com/virtualboardgames/ciudadanos/Variablesdejuego.java
@@ -6,30 +6,65 @@ public static final Variablesdejuego variablesdejuego = new Variablesdejuego();
 	
 //Tipos de variables provisionales. En muchos casos, lo más sensato será crear vectores
 	
-//Variables para almacen
-//PM: Producción mensual
-//GM: Gasto mensual
-//NM: Neto mensual
-//PA: Producción anual
-//GA: Gasto anual
-//NA: Neto anual
-//RE: Reservas
-//CP: Próxima caducidad
 
-//Trigo en almacén
-int pmtrigo, gmtrigo, nmtrigo, patrigo, gatrigo, natrigo, retrigo, cptrigo = 0;
+
+
+Almacen almacen = new Almacen();
+
+public class Almacen{
+
+	//Variables para almacen
+	//PM: Producción mensual
+	//GM: Gasto mensual
+	//NM: Neto mensual
+	//PA: Producción anual
+	//GA: Gasto anual
+	//NA: Neto anual
+	//RE: Reservas
+	//CP: Próxima caducidad
+	
+	
+	
+//ALMACÉN
+String [] stringsdealmacen;
+int[] intsdealmacen;
+	
+
+//Trigo en almacén (8 cada una: PM, GM, NM, PA, GA, NA, RE, CP)
 //Fruta en almacén
-int pmfruta, gmfruta, nmfruta, pafruta, gafruta, nafruta, refruta, cpfruta = 0;
 //Carne en almacén
-int pmcarne, gmcarne, nmcarne, pacarne, gacarne, nacarne, recarne, cpcarne = 0;
 //Carne salada en almacén
-int pmcarnesal, gmcarnesal, nmcarnesal, pacarnesal, gacarnesal, nacarnesal, 
-recarnesal, cpcarnesal = 0; 
 //Pescado en almacén
-int pmpesc, gmpesc, nmpesc, papesc, gapesc, napesc, repesc, cppesc = 0;
 //Miel en almacén
-int pmmiel, gmmiel, nmmiel, pamiel, gamiel, namiel, remiel, cpmiel = 0;
 
+
+public Almacen(){
+	stringsdealmacen = new String[16];
+	stringsdealmacen[0] = "Almacén";
+	stringsdealmacen[1] = "";
+	stringsdealmacen[2] = "Prod. mes";
+	stringsdealmacen[3] = "Gasto. mes";
+	stringsdealmacen[4] = "Neto mes";
+	stringsdealmacen[5] = "Prod. año";
+	stringsdealmacen[6] = "Gasto año";
+	stringsdealmacen[7] = "Neto año";
+	stringsdealmacen[8] = "Reservas";
+	stringsdealmacen[9] = "Prox. Caduc.";
+	stringsdealmacen[10] = "Trigo";
+	stringsdealmacen[11] = "Fruta";
+	stringsdealmacen[12] = "Carne";
+	stringsdealmacen[13] = "Carne sal.";
+	stringsdealmacen[14] = "Pescado";
+	stringsdealmacen[15] = "Miel";
+	
+	intsdealmacen = new int[48];
+	for (int i=0; i<48; i++){
+		intsdealmacen[i] = 0;
+	}
+	
+}
+
+}
 
 private Variablesdejuego(){
 	
